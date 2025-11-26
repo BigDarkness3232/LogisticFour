5958e4abcfbc64015561f4534f74d573302dfb83
Antonio <aantonio.martinezc@gmail.com>
2025-11-25 23:38:59 -0300

    update

--- LogisticFour/core/urls.py
+++ LogisticFour/core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # App principal
     path("", views.dashboard, name="dashboard"),
     path("productos/", views.product_list, name="products"),
-    path("productos/", views.product_list, name="products"),
+   
     path("category/<slug:slug>/", views.category, name="category"),
     path("products/add/", views.product_add, name="product_add"),
     path("productos/<int:pk>/etiqueta/", views.etiqueta_producto, name="etiqueta_producto"),
